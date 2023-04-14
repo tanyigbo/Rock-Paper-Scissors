e495781cae27cd52a4eda2a3efc3216bca85ec0e
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-14 05:06:26 -0500

    added player fields and methods

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,6 +1,31 @@
 package player;
 
+import java.util.List;
+
 public abstract class Player {
     private String playerName;
+    private int winCount;
+    private int loseCount;
+    private List<List<String>> gameHistory;
 
+    public String getPlayerName(){
+        return playerName;
+    }
+    public void setPlayerName(String name){
+        this.playerName = name;
+    }
+    public int getWinCount(){
+        return winCount;
+    }
+    public void addWin(){
+        winCount++;
+    }
+    public int getLoseCount(){
+        return loseCount;
+    }
+    public void addLose(){
+        loseCount++;
+    }
+
+    public abstract int chooseWeapon();
 }
