a10ab3a90b9f8b80a79155c251d5f810317fc464
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-14 09:05:47 -0500

    finalized game class

---
+++ src/main/java/Game.java
@@ -0,0 +1,92 @@
+import player.Player;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+public class Game {
+
+    private static List<String> player0GameOptions = Arrays.asList("Win", "Lose");
+    private static List<String> player1GameOptions = Arrays.asList("Lose", "Win");
+    boolean hasBothPlayers;
+    private List<Player> players;
+    private Choices playerWeaponOptions = new Choices();
+
+    public Game() {
+        hasBothPlayers = false;
+    }
+
+    public void addPlayer(Player player) {
+        if (players.size() < 2) {
+            players.add(player);
+            checkHasTwoPlayers();
+        }
+    }
+
+    public Player getPlayer(int playerID) {
+        if (playerID > -1 && playerID < players.size()) {
+            return players.get(playerID);
+        }
+        return null;
+    }
+
+    public void checkHasTwoPlayers() {
+        hasBothPlayers = players.size() == 2 ? true : false;
+        if (hasBothPlayers) {
+            checkPlayerNameCollision();
+        }
+    }
+
+    public void checkPlayerNameCollision() {
+        if (players.get(0).getPlayerName().equals(players.get(1).getPlayerName())) {
+            hasBothPlayers = false;
+            players.remove(1);
+        }
+    }
+
+    public int playRound() {
+        int player0Choice = players.get(0).chooseWeapon();
+        int player1Choice = players.get(1).chooseWeapon();
+        if (player0Choice < 3 && player1Choice < 3) {
+            int roundResult = determineWinner(player0Choice, player1Choice);
+            updatePlayerRecord(player0Choice, player1Choice, roundResult);
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * determineWinner takes the players inputs as two integer arguments
+     * to determines the winner of the round by calculating the value difference
+     * {0=Rock, 1=Paper, 2=Scissors}
+     *
+     * @param input0 : integer value for player 1's choice
+     * @param input1 : integer value for player 1's choice
+     * @return the winner of the round or 2 if it is a draw
+     */
+    public int determineWinner(int input0, int input1) {
+        switch (input0 - input1) {
+            case -2, 1:
+                return 0;
+            case -1, 2:
+                return 1;
+            default:
+                return 2;
+        }
+    }
+
+    public void updatePlayerRecord(int player0Choice, int player1Choice, int winner) {
+        String gameTimeStamp = LocalDateTime.now().toString();
+        players.get(0).addGameToHistory(
+                gameTimeStamp,
+                players.get(1).getPlayerName(),
+                player0GameOptions.get(winner),
+                playerWeaponOptions.getWeapon(player0Choice));
+        players.get(1).addGameToHistory(
+                gameTimeStamp,
+                players.get(0).getPlayerName(),
+                player1GameOptions.get(winner),
+                playerWeaponOptions.getWeapon(player1Choice));
+    }
+}
