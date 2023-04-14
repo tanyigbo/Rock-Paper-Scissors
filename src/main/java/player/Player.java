25f5e11aacae113100ccf2e72d6997edcb8b572b
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-14 09:04:58 -0500

    finalized player abstract class

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,31 +1,59 @@
 package player;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class Player {
     private String playerName;
+    private String relativeSaveLocation;
     private int winCount;
     private int loseCount;
-    private List<List<String>> gameHistory;
+    private List<List<String>> gameHistory = new ArrayList<>();
 
-    public String getPlayerName(){
+    public String getPlayerName() {
         return playerName;
     }
-    public void setPlayerName(String name){
+
+    public void setPlayerName(String name) {
         this.playerName = name;
+        this.relativeSaveLocation = name + ".txt";
     }
-    public int getWinCount(){
+
+    public String getRelativeSaveLocation() {
+        return relativeSaveLocation;
+    }
+
+    public int getWinCount() {
         return winCount;
     }
-    public void addWin(){
+
+    private void addWin() {
         winCount++;
     }
-    public int getLoseCount(){
+
+    public int getLoseCount() {
         return loseCount;
     }
-    public void addLose(){
+
+    private void addLose() {
         loseCount++;
     }
+
+    public void addGameToHistory(String dateTime, String opponent, String roundResult, String weapon) {
+        List<String> gameData = Arrays.asList(dateTime, opponent, roundResult, weapon);
+        gameHistory.add(gameData);
+        if (roundResult.equals("Win")) {
+            addWin();
+        } else {
+            addLose();
+        }
+    }
+
+    public List<List<String>> getGameHistory() {
+        return gameHistory;
+    }
+
 
     public abstract int chooseWeapon();
 }
