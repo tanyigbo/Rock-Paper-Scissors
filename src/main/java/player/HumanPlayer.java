590f409492b7f503187fb150b879c3f3f52cdef4
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-14 09:05:22 -0500

    added input methods for human player

--- src/main/java/player/HumanPlayer.java
+++ src/main/java/player/HumanPlayer.java
@@ -1,4 +1,37 @@
 package player;
 
-public class HumanPlayer {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class HumanPlayer extends Player {
+
+    private final List<String> validResponses = Arrays.asList("rock", "paper", "scissor", "quit");
+    private boolean givenValidResponse;
+    private int usersChoice;
+
+    @Override
+    public int chooseWeapon() {
+        Scanner playerIn = new Scanner(System.in);
+        givenValidResponse = false;
+        while (!givenValidResponse) {
+            System.out.println("Type 'rock', 'paper', or 'scissors' to play.");
+            System.out.println("Type 'quit' to go back to the main menu.");
+            String userInput = playerIn.nextLine();
+            checkResponse(userInput);
+        }
+        playerIn.close();
+        return usersChoice;
+    }
+    private void checkResponse(String userInput){
+        userInput = userInput.replaceAll("\\s", "").toLowerCase();
+        if(validResponses.contains(userInput)){
+            givenValidResponse = true;
+            usersChoice = validResponses.indexOf(userInput);
+        }
+        else {
+            System.out.println("\n Please input a valid response.");
+        }
+    }
+
 }
