5dfb70989532b47f8b8b1d01dc276c9eca944fa0
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-14 11:05:25 -0500

    added CPU name to class

--- src/main/java/player/CPUPlayer.java
+++ src/main/java/player/CPUPlayer.java
@@ -2,6 +2,9 @@ package player;
 
 public class CPUPlayer extends Player{
 
+    public CPUPlayer(){
+        setPlayerName("CPU");
+    }
     @Override
     public int chooseWeapon() {
         return (int)(Math.random()*2);
