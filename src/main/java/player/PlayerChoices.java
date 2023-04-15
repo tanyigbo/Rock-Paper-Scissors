82ceff3e2d7ec198c27790f971d48eea73babfc5
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-15 07:19:04 -0500

    added interface for game choices

---
+++ src/main/java/player/PlayerChoices.java
@@ -0,0 +1,8 @@
+package player;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface PlayerChoices {
+    static final List<String> playerChoices = Arrays.asList("rock","paper","scissors");
+}
