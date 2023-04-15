edf2dc74d8e7b90b3c70943ad0d00c0fb5b1a180
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-15 07:20:53 -0500

    HumanPlayer implements two interfaces

--- src/main/java/player/HumanPlayer.java
+++ src/main/java/player/HumanPlayer.java
@@ -1,33 +1,37 @@
 package player;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
-public class HumanPlayer extends Player {
+public class HumanPlayer extends Player implements PlayerChoices,ScannerInput {
 
-    private final List<String> validResponses = Arrays.asList("rock", "paper", "scissor", "quit");
     private boolean givenValidResponse;
     private int usersChoice;
 
+    public HumanPlayer(String name){
+        setPlayerName(name);
+    }
+
     @Override
     public int chooseWeapon() {
-        Scanner playerIn = new Scanner(System.in);
         givenValidResponse = false;
         while (!givenValidResponse) {
             System.out.println("Type 'rock', 'paper', or 'scissors' to play.");
             System.out.println("Type 'quit' to go back to the main menu.");
-            String userInput = playerIn.nextLine();
+            String userInput = input.nextLine();
             checkResponse(userInput);
         }
-        playerIn.close();
         return usersChoice;
     }
+
     private void checkResponse(String userInput){
         userInput = userInput.replaceAll("\\s", "").toLowerCase();
-        if(validResponses.contains(userInput)){
+        if(userInput.equals("quit")){
             givenValidResponse = true;
-            usersChoice = validResponses.indexOf(userInput);
+            usersChoice = 9;
+        }
+        if(playerChoices.contains(userInput)){
+            givenValidResponse = true;
+            usersChoice = playerChoices.indexOf(userInput);
         }
         else {
             System.out.println("\n Please input a valid response.");
